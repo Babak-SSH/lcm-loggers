f02849579cc31698e05d18fa42317352513ef1b1
Babak-SSH <babak.sefidgar@gmail.com>
2023-01-25 16:44:17 +0330

    Add: add some comments and removed unnecessary imports and variables. Modify: error handling should catch most of the errors

--- read_imu.py
+++ read_imu.py
@@ -1,6 +1,5 @@
 import serial
 import time
-import numpy as np
 import sys
 
 sys.path.insert(1, '/home/pi/projects/test_lcm/iust-quadruped/control/')
@@ -10,43 +9,42 @@ import lcm
 
 from robot_types import IMU_t
 
-
+# initializing lcm and IMU_t data type
 lc = lcm.LCM()
 resp = IMU_t()
 
+# initializing serial communication between rpi and arduino (ttyACM0 is serial device name for Arduino)
 ser = serial.Serial('/dev/ttyACM0', 2000000)
+# flushing input buffer
 ser.reset_input_buffer()
-print("IMU ready.")
+print("IMU ready!")
 
-flag=0
 while True :
-    time.sleep(0.0008)
     try:
-        while(flag==0):
-            if ser.read()==b'\n':
-                flag+=1
-                break
-
+        # data rate is about 5khz, with this sleep we reduce it to 1khz so we data every one milisecond
+        time.sleep(0.0008)
         if ser.in_waiting > 0:
             data = ser.readline()
 
-        try:
-            acc_x, acc_y, acc_z = map(float, str(data)[2:-4].lstrip().replace("  ", " ").split(" "))
-            resp.acc_x = acc_x
-            resp.acc_y = acc_y
-            resp.acc_z = acc_z
+            try:
+                # removing whitespaces and spliting the numbers
+                acc_x, acc_y, acc_z = map(float, str(data)[2:-4].lstrip().replace("  ", " ").split(" "))
+                resp.acc_x = acc_x
+                resp.acc_y = acc_y
+                resp.acc_z = acc_z
 
-            lc.publish("IMU_ACC", resp.encode())
+                # publish response data to lcm
+                lc.publish("IMU_ACC", resp.encode())
 
-        except Exception as e:
-            print("error!")
-            if len(data) != 0:
-                print(str(data[-1]))
-            else:
-                print("empty data recieved!")
-            print(e)
-            pass
+            except Exception as e:
+                print("IMU Error!")
+                if len(data) != 0:
+                    print(str(data[-1]))
+                else:
+                    print("Empty data recieved!")
+                print(e)
+                pass
 
     except KeyboardInterrupt:
-        print("program closed manually!")
-        sys.exit(1)
+        print("IMU closed manually!")
+        sys.exit()
