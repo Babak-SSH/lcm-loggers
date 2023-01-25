5309e73e684d9f922479e90751ac3341311a0efc
Babak-SSH <babak.sefidgar@gmail.com>
2023-01-25 16:54:21 +0330

    Add: add comments and error handling.

--- read_contact.py
+++ read_contact.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-
 import sys
 
 sys.path.insert(1, '/home/pi/projects/test_lcm/iust-quadruped/control/')
@@ -11,18 +10,25 @@ import lcm
 from robot_types import contact_t
 
 
+# initializing lcm and contact_t data type
 lc = lcm.LCM()
 resp = contact_t()
 
 GPIO.setmode(GPIO.BOARD)
 contact_pin = 13
 GPIO.setup(contact_pin, GPIO.IN)
-
+print("Contact ready!")
 
 while(1):
-    time.sleep(0.001)
-    a=GPIO.input(contact_pin)
-    resp.touch = GPIO.input(contact_pin)
-    lc.publish("CONTACT", resp.encode())
+    try:
+        # add one millisecond sleep to synchronize it with imu data
+        time.sleep(0.001)
+        a=GPIO.input(contact_pin)
 
-GPIO.cleanup()
+        resp.touch = GPIO.input(contact_pin)
+
+        lc.publish("CONTACT", resp.encode())
+    except KeyboardInterrupt:
+        print("Contact closed manually!")
+        GPIO.cleanup()
+        sys.exit()
